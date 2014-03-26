87454aeffb73b7382cf89e4654387cd61421b4c6
Harry Braviner <harry.braviner@gmail.com>
2014-03-26 18:21:24 +0000

    TWI working
    ATmega now accepts a stream passed over TWI.
    PWM duty cycle of the two LEDs is now set to the value of the first
    two bytes read.

--- motor_PWM.c
+++ motor_PWM.c
@@ -5,9 +5,21 @@
 #include <util/delay.h>
 
 unsigned char sreg;	// uses for temporarily saving register values
+volatile uint8_t i;	// Dummy counter variable for use with data buffer
+volatile uint8_t dataRecBuff[4];	// Stores incoming data
 
 ISR(TWI_vect){
-
+	sreg = SREG; cli();	// Disable other interrupts
+	if(TWSR == 0x60) {	// SLA+W received
+		i = 0;		// Start at beginning of buffer again
+	}
+	if(TWSR == 0x80) {	// Data received and acknowledged
+		if(i<4)	// Ignore extra data
+			dataRecBuff[i] = TWDR;
+		i++;
+	}
+	TWCR &= ~(0 << TWINT);	// Clear the interrupt flag (by writing 1 to it)
+	SREG = sreg;	// Re-enable interrupts
 }
 
 void delayms(uint16_t millis){
@@ -32,26 +44,37 @@ int main(void){
 	ICR1 = 0xff;	// Also 16-bit register. Set the values of TOP to 0xff;
 	SREG = sreg;
 
+	// Empty the dataRecBuff
+	for(i=0; i<4; i++){
+		dataRecBuff[i] = 0x00;
+	}
+
+	/*
 	// Set OC1A to 20% duty cycle
 	sreg = SREG;
 	cli();
 	OCR1A = 0x13;
 	OCR1B = 0x33;
 	SREG = sreg;
+	*/
 
 	// Setup device as a slave receiver for TWI
+	DDRC &= ~(1 << DDC4); // Configure the SDA pin as a pull-up
+	PORTC |= 1 << PORTC4;
+	DDRC &= ~(1 << DDC5); // Configure the SCL pin as a pull-up
+	PORTC |= 1 << PORTC5;
 	PRR &= ~(1 << PRTWI);	// Set the PRTWI bit to zero (ie. disable power-saving mode)
 	TWAR = 0xa << 1;	// Set the address of the chip to 10, and TWGCE bit to zero (ignore general calls)
-	TWCR = 0x44;	// Enable TWI with appropriate settings for slave receiver
+	TWCR = 0x45;	// Enable TWI with appropriate settings for slave receiver, enabling interrupts
+	sei();	// Enable interrupts
 
 	while(1){	// Idle loop to test code is still running
-		// Set OC1A to higher duty cycle
+		// Set OC1A and OC1B to the duty cycles passed over TWI
 		sreg = SREG;
 		cli();
-		OCR1A = (OCR1A + 1) % 0xff;
+		OCR1A = dataRecBuff[0];
+		OCR1B = dataRecBuff[1];
 		SREG = sreg;
-		
-		delayms(5);
 	}
 
 	return 0;
