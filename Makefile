4f08156d257df21b2ed2955f29c3731192d80146
Harry Braviner <harry.braviner@gmail.com>
2014-05-07 22:32:34 +0100

    Interrupt change and Makefile fuses
    Changed interrupt disabling so that it didn't interfere with the
    RaspPi's attempts to communicate with the board.
    
    Added fuse setting in the Makefile.

--- Makefile
+++ Makefile
@@ -8,6 +8,9 @@ PROGDEVICE=/dev/ttyACM0
 
 program : $(TARGET).hex
 	$(PROG) -c avrisp2 -p m$(MEGA) -P $(PROGDEVICE) -e
+	$(PROG)	-c avrisp2 -p m$(MEGA) -P $(PROGDEVICE) -U efuse:w:0x07:m
+	$(PROG) -c avrisp2 -p m$(MEGA) -P $(PROGDEVICE) -U hfuse:w:0xd9:m
+	$(PROG) -c avrisp2 -p m$(MEGA) -P $(PROGDEVICE) -U lfuse:w:0xe2:m
 	$(PROG) -c avrisp2 -p m$(MEGA) -P $(PROGDEVICE) -U flash:w:$(TARGET).hex
 
 %.obj : %.o
--- motor_PWM.c
+++ motor_PWM.c
@@ -1,7 +1,11 @@
+// Robot control code
+// Written by Harry Braviner (harry.braviner@gmail.com)
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 #define F_CPU	8000000UL	// 8Mhz cpu
+#define	SPEED_DIR_SET_CMD	0x01	// Command to set speeds and direction of motors
 #include <util/delay.h>
 
 unsigned char sreg;	// uses for temporarily saving register values
@@ -17,6 +21,29 @@ ISR(TWI_vect){
 		if(i<4)	// Ignore extra data
 			dataRecBuff[i] = TWDR;
 		i++;
+		if((i==4) && (dataRecBuff[0]==SPEED_DIR_SET_CMD)){
+			// We have a speed and direction to write to the relevant pins
+
+			// Set the direction pins for the left motor
+			// 0bxxxxxx01 = left forwards, 0bxxxxxx10 = left backwards
+			if(dataRecBuff[1] & (1<<0))	PORTB |=  (1 << PORTB3);
+			else				PORTB &= ~(1 << PORTB3);
+			if(dataRecBuff[1] & (1<<1))	PORTB |=  (1 << PORTB4);
+			else				PORTB &= ~(1 << PORTB4);
+
+			// Set the direction pins for the right motor
+			// 0bxxxx01xx = right forwards, 0bxxxx10xx = right backwards
+			if(dataRecBuff[1] & (1<<2))	PORTB |=  (1 << PORTB0);
+			else				PORTB &= ~(1 << PORTB0);
+			if(dataRecBuff[1] & (1<<3))	PORTB |=  (1 << PORTB5);
+			else				PORTB &= ~(1 << PORTB5);
+			
+			// Set left motor speed (note that interrupts are already disabled in this routine)
+			OCR1A = dataRecBuff[2];
+			
+			// Set right motor speed
+			OCR1B = dataRecBuff[3];
+		}
 	}
 	TWCR &= ~(0 << TWINT);	// Clear the interrupt flag (by writing 1 to it)
 	SREG = sreg;	// Re-enable interrupts
@@ -30,11 +57,13 @@ void delayms(uint16_t millis){
 }
 
 int main(void){
-	// Setup clock and OC1A (pin 15)
+	// Setup clock and output pins
 	DDRB |= (1 << DDB1);	// Set PB1 (aka OC1A) as output
 	PORTB &= ~(1 << PORTB1);	// Set PB1 low
 	DDRB |= (1 << DDB2);	// Set PB2 (aka OC1B) as output
 	PORTB &= ~(1 << PORTB2);	// Set PB2 low
+	DDRB |= ((1 << DDB0) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5));	// Set PB0, PB3, PB4 and PB5 (motor direction pins) as outputs
+	PORTB &= ~((1 << PORTB0) | (1 << PORTB3) | (1 << PORTB4) | (1 << PORTB5));	// Set PB0, PB3, PB4 and PB5 (motor direction pins) to low
 
 	TCCR1A = 0xa0;	// Set OC1A & OC1B on match when upcounting
 	TCCR1B = 0x11;	// Set internal clock source, set WGM13:2 for phase & freq correct PWM mode (WGM11:0 set in TCCR1A)
@@ -49,15 +78,6 @@ int main(void){
 		dataRecBuff[i] = 0x00;
 	}
 
-	/*
-	// Set OC1A to 20% duty cycle
-	sreg = SREG;
-	cli();
-	OCR1A = 0x13;
-	OCR1B = 0x33;
-	SREG = sreg;
-	*/
-
 	// Setup device as a slave receiver for TWI
 	DDRC &= ~(1 << DDC4); // Configure the SDA pin as a non-pull-up
 	PORTC &= ~(1 << PORTC4);
@@ -70,11 +90,15 @@ int main(void){
 
 	while(1){	// Idle loop to test code is still running
 		// Set OC1A and OC1B to the duty cycles passed over TWI
-		sreg = SREG;
-		cli();
-		OCR1A = dataRecBuff[0];
-		OCR1B = dataRecBuff[1];
-		SREG = sreg;
+		//sreg = SREG;
+		//cli();
+		//OCR1A = dataRecBuff[1];
+		//SREG = sreg; // Warning - failing to re-enable the interrupt results in write failures from the Pi
+
+		//sreg = SREG;
+		//cli();
+		//OCR1B = dataRecBuff[2];
+		//SREG = sreg;
 	}
 
 	return 0;
